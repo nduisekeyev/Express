a42f2927d660157e6271a5128f22a480073bf182
Nur Duisekeyev <nur.duisekeyev@unineedgroup.com>
2023-01-09 13:52:12 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT || 4000);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+
+module.exports = app;
